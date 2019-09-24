5ec3b87136d54380d6c7f3f599aad853e445aca7
ruthbrennankk <ruthbrennankk@gmail.com>
2019-09-24 19:37:04 +0100

    ideas for project structure

--- src/lcaTest.java
+++ src/lcaTest.java
@@ -1,5 +1,19 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 class lcaTest {
+    //Error Cases
+
+        //test for empty tree - null
+
+        //test on same node - null
+
+    //Other cases
+
+        //root and other node
+
+        //parent and child node
+
+        //two child same immediate parent
+
 
 }

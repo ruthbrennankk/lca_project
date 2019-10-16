6f269ae08fa2fb3c8e607f7c273f8d518a40a012
ruthbrennankk <ruthbrennankk@gmail.com>
2019-10-16 15:04:08 +0100

    Graph Setup

---
+++ src/GraphTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+class GraphTest {
+    @Test
+    public void testGraph() {
+        //TODO
+    }
+
+    @Test
+    public void testAddEdge() {
+        //TODO
+    }
+
+    @Test
+    public void testFindLCA() {
+        //TODO
+    }
+
+}
